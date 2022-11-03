a07d531a2181e980a05b7d182144664d30c0ab67
최정욱 <@DESKTOP-RU4OGUF>
2022-11-03 10:42:07 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'developer' of https://github.com/highsprito/SoloFood.git
    into developer
    
    Conflicts:
    	HONBAB/src/main/webapp/WEB-INF/views/admin/ADMIndex.jsp

---
+++ HONBAB/src/main/java/com/web/honbab/admin/controller/AdminController.java
@@ -0,0 +1,86 @@
+package com.web.honbab.admin.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.web.honbab.admin.service.AdminService;
+import com.web.honbab.session.admin.AdminSession;
+
+
+@Controller
+@RequestMapping("admin")
+public class AdminController implements AdminSession{
+	
+	@Autowired
+	private AdminService as;
+
+	@PostMapping("admin_check") //관리자 확인
+	public String adminCheck(HttpServletRequest request, RedirectAttributes rs) {
+		int result = as.admin_check(request);
+		if(result == 0) {
+			rs.addAttribute("adminId", request.getParameter("adminId"));
+			return "redirect:successLogin";
+		}
+		return "redirect:adminlogin";
+	}	
+	
+	@PostMapping("successLogin") //로그인 성공
+	public String successLogin(@RequestParam("adminId") String adminId, HttpSession session) {
+		session.setAttribute(LOGIN, adminId);
+		return "admin/successLogin";
+	}
+	
+	@PostMapping("adminLogin")
+	public String adminLogin() {
+		return "admin/adminlogin";
+	}
+	
+	@GetMapping("adminLogout")
+	public String logout(HttpSession session) {
+		if(session.getAttribute("loginUser") != null) {
+			session.invalidate();
+		}
+		return "redirect:/index";
+	}
+	
+	@GetMapping("adminBan") //회원탈퇴권한 페이지
+	public String adminBan() {
+		return "admin/adminBan";
+	}	
+	
+	@PostMapping("general_delete") //일반회원 삭제
+	public void general_delete(@RequestParam("adminId") String id,HttpServletResponse response, HttpServletRequest request) throws IOException{
+		String message = as.generalDelete(id, request);
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();		
+		out.println(message);
+	}
+	
+	@PostMapping("partner_delete") //파트너 삭제
+	public void partner_delete(@RequestParam("adminId") String id,HttpServletResponse response, HttpServletRequest request) throws IOException{
+		String message = as.partnerDelete(id, request);
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println(message);
+	}
+	
+	
+	
+	
+	
+	
+
+}
