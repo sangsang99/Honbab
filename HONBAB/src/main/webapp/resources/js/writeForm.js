0d5403c7cac607a76b1a3515faa937cebb6a7dc1
bunnojojeoljalhae <pokari0915@naver.com>
2022-11-07 19:40:27 +0900

    home.jsp, index.jsp css 수정

--- HONBAB/src/main/webapp/resources/js/writeForm.js
+++ HONBAB/src/main/webapp/resources/js/writeForm.js
@@ -1,4 +1,8 @@
 //JOIN 클릭시, join_view로 이동
 document.querySelector(".scoll-down2").addEventListener("click", (e) => {
-  document.querySelector(".join_form_wrap").scrollIntoView({ behavior: "smooth" });
+  document.querySelector(".join").scrollIntoView({ behavior: "smooth" });
+});	
+
+document.querySelector(".scoll-down3").addEventListener("click", (e) => {
+  document.querySelector(".footer").scrollIntoView({ behavior: "smooth" });
 });	
